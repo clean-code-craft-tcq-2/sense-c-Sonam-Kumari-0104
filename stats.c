93f72bdf4943dc06697c637c82d33e6243a2c2b0
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2021-10-22 22:01:33 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,7 +7,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
    // s.average = 0;
    // s.min = 0;
    // s.max = 0;
-}struct Stats s;
+}Stats s;
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
