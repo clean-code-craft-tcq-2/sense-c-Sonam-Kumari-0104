d81ea46119efe59cce150d0eeda721b1f26a97a8
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2021-10-23 20:33:12 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -6,9 +6,19 @@ struct Stats compute_statistics(const float* numberset, int setlength)
     int i=0; 
     float sum=0; 
     
-    s.average=0;
-    s.max=numberset[0];
-    s.min=numberset[0];
+    // Setting all fields of computedStats (average, max, min) to NAN in case of empty array
+    if (numberset==0)
+    {
+       s.average=0.0/0.0;
+       s.max=0.0/0.0;
+       s.min=0.0/0.0;
+    }
+   else
+   {
+        s.average=0;
+        s.max=numberset[0];
+        s.min=numberset[0];
+   }
    
     //Average calculation
     for (i=0; i<setlength; i++)
