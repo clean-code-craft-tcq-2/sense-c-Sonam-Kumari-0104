8cbfaa0d46d2fd5bfe3abe09c601e7a8c280cd3a
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2021-10-23 20:22:50 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -18,7 +18,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 {
   if(computedStats.max>maxThreshold)
   {
-   if (&alerters==emailAlerter)
+   if (alerters==&emailAlerter)
    {
      emailAlerter();
    }
